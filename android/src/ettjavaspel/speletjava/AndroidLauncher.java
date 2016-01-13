b0c0bf3b961ebf8b0174707b60a071929b3d25b8
Oskari Rundgren <orundg@gmail.com>
2016-01-13 19:10:48 +0100

    Empty project.

---
+++ android/src/ettjavaspel/speletjava/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ettjavaspel.speletjava;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ettjavaspel.speletjava.SpeletJava;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpeletJava(), config);
+	}
+}
